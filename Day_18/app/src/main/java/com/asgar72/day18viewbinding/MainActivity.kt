4c312eaa9c53a84df05293340914927095b6afc8
asgar72 <72asgar72@gmail.com>
2023-08-22 18:34:12 +0530

    view binding

--- Day_18/app/src/main/java/com/asgar72/day18viewbinding/MainActivity.kt
+++ Day_18/app/src/main/java/com/asgar72/day18viewbinding/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.asgar72.day18viewbinding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.asgar72.day18viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.txtView.setOnClickListener {
-
+        binding.button.setOnClickListener {
+            if (binding.checkBox.isChecked) {
+                val intent = Intent(this, Activity2::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please accept T&C", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
