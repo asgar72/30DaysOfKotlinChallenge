004d21e495eb1d1c7668624c37333f9136634724
asgar72 <72asgar72@gmail.com>
2023-08-22 11:41:21 +0530

    View binding

--- Day_18/app/src/main/java/com/asgar72/day18viewbinding/MainActivity.kt
+++ Day_18/app/src/main/java/com/asgar72/day18viewbinding/MainActivity.kt
@@ -2,10 +2,20 @@ package com.asgar72.day18viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asgar72.day18viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtView.setOnClickListener {
+
+        }
+
     }
 }
