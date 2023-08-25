0117483f4b4bd53d40d788afe5c2f5be829c0bf3
asgar72 <72asgar72@gmail.com>
2023-08-25 18:03:45 +0530

    dialog box

--- Day_19/app/src/main/java/com/asgar72/dailogbox/MainActivity.kt
+++ Day_19/app/src/main/java/com/asgar72/dailogbox/MainActivity.kt
@@ -2,10 +2,20 @@ package com.asgar72.dailogbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asgar72.dailogbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn1.setOnClickListener{
+
+        }
+
+
     }
 }
