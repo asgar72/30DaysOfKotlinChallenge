c52cacdac2750ad1cd7eb56c454d2978630fb733
asgar72 <72asgar72@gmail.com>
2023-08-29 18:25:05 +0530

    customized alert dialog box

--- Day_20/app/src/main/java/com/asgar72/customizedalertbox/MainActivity.kt
+++ Day_20/app/src/main/java/com/asgar72/customizedalertbox/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.asgar72.customizedalertbox
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    //Create a variable of type Dialog
+    lateinit var dialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnClick = findViewById<Button>(R.id.btnClick)
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.custom_dialog)
+        dialog.window?.setBackgroundDrawable(getDrawable(R.drawable.bg_alertbox))
+
+        //creating variable for the custom_dialog.xml design
+        var buttonGood = dialog.findViewById<Button>(R.id.btnGood)
+        var buttonFeedback = dialog.findViewById<Button>(R.id.btnFeedback)
+
+        buttonGood.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        buttonFeedback.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data=Uri.parse("https://72asgar72@gmail.com")
+            startActivity(intent)
+        }
+
+        btnClick.setOnClickListener {
+            dialog.show()
+        }
     }
 }
