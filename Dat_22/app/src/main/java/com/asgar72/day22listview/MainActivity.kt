cb568d9489f56b4c08d583f22057e707ba0737ee
asgar72 <72asgar72@gmail.com>
2023-08-31 11:01:19 +0530

    listView completed

--- Dat_22/app/src/main/java/com/asgar72/day22listview/MainActivity.kt
+++ Dat_22/app/src/main/java/com/asgar72/day22listview/MainActivity.kt
@@ -2,6 +2,10 @@ package com.asgar72.day22listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import com.asgar72.day22listview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +16,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.listView.
+        supportActionBar?.hide()
+
+        val taskList = arrayListOf<String>()
+
+        taskList.add("Attend Meeting")
+        taskList.add("Work on resume")
+        taskList.add("Complete App Dev project")
+        taskList.add("Buy  a book from market")
+        taskList.add("Improve internet presences")
+        taskList.add("Watching movie")
+        taskList.add("Meet with new friends")
+        taskList.add("Dinner with asif")
+        taskList.add("Playing Valorant")
+        taskList.add("Push code on Github")
+
+        val adapterForMylsitView = ArrayAdapter(this,android.R.layout.simple_list_item_1,taskList)
+        binding.listView.adapter = adapterForMylsitView
+
+        binding.listView.setOnItemClickListener { parent, view, position, id ->
+
+            val text = "Clicked on Item : " + (view as TextView).text.toString()
+            Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
