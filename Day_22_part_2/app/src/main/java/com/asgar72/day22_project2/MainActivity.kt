2bc6887e03bf24cdc07287e666e3d5d0f5584a6f
asgar72 <72asgar72@gmail.com>
2023-09-01 10:55:08 +0530

    customized list vview

--- Day_22_part_2/app/src/main/java/com/asgar72/day22_project2/MainActivity.kt
+++ Day_22_part_2/app/src/main/java/com/asgar72/day22_project2/MainActivity.kt
@@ -2,11 +2,31 @@ package com.asgar72.day22_project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var userArrayList : ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
+
+        val name = arrayOf("Irshad","Neeraj","SRK","Asgar","Shajar")
+        val lastMsg = arrayOf("Hello bro","Good Morning","Hii ..","Yes bro","I'm fine.")
+        val lastMsgTime = arrayOf("11:30 AM","8:10 AM","02:00 PM","10:50 PM","10:59 PM")
+        val phoneNumber = arrayOf("6389261616","9948562145","7048165235","8541230349","7789261420")
+        val imgId = intArrayOf(R.drawable.irshad,R.drawable.neeraj_chopra,R.drawable.srk,R.drawable.asgar,R.drawable.sh)
+
+        userArrayList = ArrayList()
+
+        for(eachIndex in name.indices) {
+            val user = User(name[eachIndex], lastMsg[eachIndex], lastMsgTime[eachIndex],phoneNumber[eachIndex],imgId[eachIndex])
+            userArrayList.add(user)
+        }
+
+        val listView = findViewById<ListView>(R.id.listView)
+
+        listView.isClickable =true
+        listView.adapter= MyAdapter(this,userArrayList)
+
     }
 }
---
+++ Day_22_part_2/app/src/main/java/com/asgar72/day22_project2/User.kt
@@ -0,0 +1,7 @@
+package com.asgar72.day22_project2
+
+data class User(var name: String,
+                var lastMsg: String,
+                var lastMsgTime: String,
+                var phoneNumber: String,
+                var imageId: Int)
