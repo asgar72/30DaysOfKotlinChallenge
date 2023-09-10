cc37612bc0048254586bf98025f9f7523773bd7e
asgar72 <72asgar72@gmail.com>
2023-09-10 12:20:55 +0530

    step 4

--- Day_26/app/src/main/java/com/asgar72/api/MainActivity.kt
+++ Day_26/app/src/main/java/com/asgar72/api/MainActivity.kt
@@ -2,11 +2,37 @@ package com.asgar72.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
+        val  retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getProductData()
+
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                // if api call is a success, then use the data of API and show in your app
+
+
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                // if api call fails
+                Log.d("Main Activity","onFailure"+ t.message)
+            }
+        })
+        
     }
 }
