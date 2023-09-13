e35446eefde4c83b832a93e92b7ae15cefd98ae7
asgar72 <72asgar72@gmail.com>
2023-09-14 02:58:04 +0530

    Notifications completed

--- Day_27/app/src/main/java/com/asgar72/day27notifications/MainActivity.kt
+++ Day_27/app/src/main/java/com/asgar72/day27notifications/MainActivity.kt
@@ -3,7 +3,9 @@ package com.asgar72.day27notifications
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -25,11 +27,16 @@ class MainActivity : AppCompatActivity() {
 
         createNotificationChannel()
 
+        //pending intent
+        val intent = Intent(this,MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_MUTABLE)
+
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("This is tittle")
             .setContentText("This is contentText")
             .setSmallIcon(R.drawable.baseline_insert_emoticon_24)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
